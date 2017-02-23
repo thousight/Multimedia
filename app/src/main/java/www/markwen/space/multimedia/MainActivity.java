5dfebb60c2f5db56959a2375b2cfba383e550109
Mark Wen <markwenguojie94@gmail.com>
2017-02-23 00:33:58 -0500

    Finish taking photo and saving

--- app/src/main/java/www/markwen/space/multimedia/MainActivity.java
+++ app/src/main/java/www/markwen/space/multimedia/MainActivity.java
@@ -1,5 +1,8 @@
 package www.markwen.space.multimedia;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Request permission
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            int ReadStoragePermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            int WriteStoragePermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int CameraPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+            int RecordAudioPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+
+            if (ReadStoragePermissionCheck == -1 || WriteStoragePermissionCheck == -1 || CameraPermissionCheck == -1 || RecordAudioPermissionCheck == -1) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO
+                }, 1);
+            }
+        }
+
 
         // Get widgets
         tabLayout = (SmartTabLayout)findViewById(R.id.tabLayout);
--- app/src/main/java/www/markwen/space/multimedia/fragments/FeaturesFragment.java
+++ app/src/main/java/www/markwen/space/multimedia/fragments/FeaturesFragment.java
@@ -1,13 +1,28 @@
 package www.markwen.space.multimedia.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import www.markwen.space.multimedia.R;
+
+import static android.os.Environment.getExternalStorageDirectory;
 
 /**
  * Created by markw on 2/22/2017.
@@ -16,6 +31,8 @@ import www.markwen.space.multimedia.R;
 public class FeaturesFragment extends Fragment {
 
     FrameLayout cameraCard, camcorderCard, micCard;
+    final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,7 +45,13 @@ public class FeaturesFragment extends Fragment {
         cameraCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                currentPhotoPath = getExternalStorageDirectory() + "/Multimedia/" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date()) + ".jpg";
+                File photo = new File(currentPhotoPath);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(getContext(), "www.markwen.space.fileprovider", photo));
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
 
@@ -49,4 +72,16 @@ public class FeaturesFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == -1) {
+            Uri image = data.getData();
+            if (image != null) {
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(image);
+                getActivity().sendBroadcast(mediaScanIntent);
+                Toast.makeText(getContext(), "Image saved to gallery", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
